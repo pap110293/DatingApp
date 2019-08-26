0668cc11bfe570a0cf5b928a72963a2f533ab0b7
A Ây <pap110293@gmail.com>
2019-08-27 06:52:10 +0700

    update

--- DatingApp.API/DTOs/UserForRegisterDto.cs
+++ DatingApp.API/DTOs/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Password must between 5 to 20 characters")]
         public string Password { get; set; }
     }
 }
