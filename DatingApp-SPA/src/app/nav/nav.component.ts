5a331c27ea29ecc0dca37647f06064f92c2f57e9
Pham Anh <pap110293@gmail.com>
2019-08-28 15:56:47 +0700

    routing

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) {}
 
   ngOnInit() {}
@@ -34,7 +36,8 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.authService.logout();
-    this.modelLogin = {};
     this.alertify.message('logged out');
+    this.modelLogin = {};
+    this.router.navigate(['/home']);
   }
 }
