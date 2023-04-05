d1243cfd6b97c8754b02eabd34db3a91a80550fb
anh.pham <pap110293@gmail.com>
2023-04-05 21:13:44 +0700

    chap 15

---
+++ new/DatingApp/DatingApp.Api/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using DatingApp.Api.Data;
+using DatingApp.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        var users = await _context.AppUsers.ToListAsync();
+        
+        return Ok(users);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+        var user = await _context.AppUsers.FindAsync(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(user);
+    }
+}
